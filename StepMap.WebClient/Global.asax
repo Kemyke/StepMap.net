928e7febedad8d7f7f60902bc1d275dd8dd2bbc4
Kemyke <kemyyy@gmail.com>
2015-11-09 21:15:02 +0100

    Project skeleton

---
+++ StepMap.WebClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StepMap.WebClient.MvcApplication" Language="C#" %>
---
+++ StepMap.WebClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StepMap.WebClient.Startup))]
+namespace StepMap.WebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
