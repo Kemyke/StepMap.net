35917ea4201e436dfd6587fd7db15bd9f45bebb1
Kemyke <kemyyy@gmail.com>
2015-11-18 23:16:40 +0100

    Add api address config Refactor controller

--- StepMap.WebClient/Controllers/HomeController.cs
+++ StepMap.WebClient/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using StepMap.WebClient.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -12,15 +13,19 @@ namespace StepMap.WebClient.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private static string ApiAddress = ConfigurationManager.AppSettings["ApiAddress"];
+        private static string Resource = "/projects";
+        private static string Address = ApiAddress + Resource;
+
+        private UserStepMapViewModel GetProjects()
         {
-            ViewBag.Title = "Stepmap";
-            var vm = new UserStepMapViewModel { Projects = new List<ServiceContracts.DTO.Project>() };
-            for (int i = 0; i < 7; i++)
-            {
-                vm.Projects.Add(null);
-            }
-            return View(vm);
+            var client = CreateClient();
+            var json = client.DownloadString(Address);
+            var projects = System.Web.Helpers.Json.Decode<List<Project>>(json);
+
+            var vm = new UserStepMapViewModel();
+            vm.Projects = projects;
+            return vm;
         }
 
         private System.Net.WebClient CreateClient()
@@ -32,19 +37,26 @@ namespace StepMap.WebClient.Controllers
             return client;
         }
 
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Stepmap";
+            var vm = GetProjects();
+            return View(vm);
+        }
+
         public ActionResult SetProjectName(int projectId, string name)
         {
             var c = CreateClient();
-            var x = c.DownloadString("http://localhost:55300/Service.svc/projects");
+            var x = c.DownloadString(Address);
             var z = System.Web.Helpers.Json.Decode<List<Project>>(x);
             var p = z.Single(tp => tp != null && tp.Id == projectId);
 
             var client = CreateClient();
             p.Name = name;
             string json = System.Web.Helpers.Json.Encode(p);
-            client.UploadString("http://localhost:55300/Service.svc/projects", "PUT", json);            
-            
-            return View("Index", new UserStepMapViewModel { Projects = new List<ServiceContracts.DTO.Project>() });
+            client.UploadString(Address, "PUT", json);
+
+            return View("Index", GetProjects());
         }
 
         public ActionResult CloseProject(int projectId)
@@ -52,14 +64,14 @@ namespace StepMap.WebClient.Controllers
             var client = CreateClient();
             string json = System.Web.Helpers.Json.Encode(projectId);
 
-            client.UploadString("http://localhost:55300/Service.svc/projects", "DELETE", json);
-            return View("Index", new UserStepMapViewModel { Projects = new List<ServiceContracts.DTO.Project>() });
+            client.UploadString(Address, "DELETE", json);
+            return View("Index", GetProjects());
         }
 
         public ActionResult CloseStep(int projectId)
         {
             var c = CreateClient();
-            var x = c.DownloadString("http://localhost:55300/Service.svc/projects");
+            var x = c.DownloadString(Address);
             var z = System.Web.Helpers.Json.Decode<List<Project>>(x);
             var p = z.Single(tp => tp != null && tp.Id == projectId);
 
@@ -74,8 +86,8 @@ namespace StepMap.WebClient.Controllers
 
             string json = System.Web.Helpers.Json.Encode(p);
 
-            client.UploadString("http://localhost:55300/Service.svc/projects", "PUT", json);
-            return View("Index", new UserStepMapViewModel { Projects = new List<ServiceContracts.DTO.Project>() });
+            client.UploadString(Address, "PUT", json);
+            return View("Index", GetProjects());
         }
 
         public ActionResult CreateProject(int position)
@@ -93,8 +105,8 @@ namespace StepMap.WebClient.Controllers
             };
             string json = System.Web.Helpers.Json.Encode(p);
 
-            client.UploadString("http://localhost:55300/Service.svc/projects", "POST", json);
-            return View("Index", new UserStepMapViewModel { Projects = new List<ServiceContracts.DTO.Project>() });
+            client.UploadString(Address, "POST", json);
+            return View("Index", GetProjects());
         }
     }
 }
